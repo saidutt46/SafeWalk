08a20803855c058806c7608d303dfcaf3ad43acb
SAI DUTT <duttganduri46@gmail.com>
2024-08-05 15:45:54 -0700

    Initial Commit

---
+++ SafeWalk/SafeWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeWalkApp.swift
+//  SafeWalk
+//
+//  Created by Sai Dutt Ganduri on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
